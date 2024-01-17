c7a82bb57e0c088ee73671facb437b777be4162c
Azhar Amir Kimanje <77221185+kimanjea@users.noreply.github.com>
2024-01-17 12:01:38 -0800

    yep

--- Program.cs
+++ Program.cs
@@ -15,16 +15,16 @@ class Program {
 
     public static int [] RotateArray(int [] array , int k) {
 
-        if (array == null || array.Length == 0 || k < 0) {
-            throw new ArgumentException("Invalid argument");
+        if (array == null || array.Length == 0 || k < 0) { // we check the edge cases of the array for when it is null or length is 0
+            throw new ArgumentException("Invalid argument"); // we throw a custom exception
         }
 
-        if(array.Distinct().Count() == 1) {
+        if(array.Distinct().Count() == 1) { // Distinct returns a new collection with no duplicates, if the count is 1 then all the elements are the same
             return array;
         }
 
-        k = k % array.Length;
+        k = k % array.Length; // we get the remainder of the array length and k, this is to avoid unnecessary rotations
 
-        return array.Skip(array.Length - k).Concat(array.Take(array.Length - k)).ToArray();
+        return array.Skip(array.Length - k).Concat(array.Take(array.Length - k)).ToArray(); // we skip the first n elements and concat both skips and take
     }
 }
