d7bf975aeedb55fbe59c32f19c2d8cb9ab56e1d4
Azhar Amir Kimanje <77221185+kimanjea@users.noreply.github.com>
2024-01-17 11:50:42 -0800

    Cyclic array
    edge cases are when the array is the same
    array is null
    array is 0
     % helps avoid unnecessary rotations
    
    practice for that return is rerurn array.Skip(array.Length - k).Concat(array.Take(array.length -k)).ToArray();

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello World!");
+
+        var array = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+        int k = 3;
+        
+        int [] result = RotateArray(array, k);
+    }
+
+    public static int [] RotateArray(int [] array , int k) {
+
+        if (array == null || array.Length == 0 || k < 0) {
+            throw new ArgumentException("Invalid argument");
+        }
+
+        if(array.Distinct().Count() == 1) {
+            return array;
+        }
+
+        k = k % array.Length;
+
+        return array.Skip(array.Length - k).Concat(array.Take(array.Length - k)).ToArray();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Rotate Array K Times 
  Codility Practice
+
+
+# Edge Cases
+the edge cases are if array is null " we can use a throw new argumentNullexception(nameof(A))"
+if the array has the same elements at every index
+
+# Avoid unnecessary rotations
+K = K % array.length 
+
+# return a concatenation of both skip and take
+we can use return array.Skip(array.length -k).Concat(array.Take(array.Length-k).Concat()).ToArray();
